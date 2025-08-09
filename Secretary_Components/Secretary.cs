5bf3ef1dc7931c37c11be81393a01186cbc858cf
bodoz <118077974+zainea-bogdan@users.noreply.github.com>
2025-08-09 09:53:07 +0300

    Add project files.

---
+++ Secretary_Components/Secretary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Students_Management.Secretary_Components
+{
+    internal class Secretary
+    {
+    }
+}
---
+++ Students_Components/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Students_Management.Students_Components
+{
+    internal class Student
+    {
+    }
+}
